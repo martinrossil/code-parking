71d9798b2d35cdf6f6ffa48d33b725b2afca8f23
Martin Rossil <martinrossil@gmail.com>
2022-04-07 12:59:09 +0200

    Moved files to a parked state.

---
+++ src/dto/IMovie.ts
@@ -0,0 +1,4 @@
+export default interface IMovie {
+    uid: string;
+    title: string;
+}
---
+++ src/dto/Movie.ts
@@ -0,0 +1,6 @@
+import IMovie from './IMovie';
+
+export default class Movie implements IMovie {
+    public uid = '';
+    public title = '';
+}
